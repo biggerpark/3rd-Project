f84d6db0f67c75e7eb7046d6449d0f957be0af0a
yoonseoq <sysooo18@naver.com>
2025-02-18 15:50:09 +0900

    0212 매출조회하는거 jwt적용

--- src/main/java/com/green/jobdone/business/model/get/BusinessGetMonthlyReq.java
+++ src/main/java/com/green/jobdone/business/model/get/BusinessGetMonthlyReq.java
@@ -1,5 +1,6 @@
 package com.green.jobdone.business.model.get;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
@@ -7,6 +8,9 @@ import lombok.Setter;
 @Getter
 @Setter
 public class BusinessGetMonthlyReq {
+    @JsonIgnore
+    private long signedUserId;
+
     @Schema(title = "businessId")
     private long businessId;
 
