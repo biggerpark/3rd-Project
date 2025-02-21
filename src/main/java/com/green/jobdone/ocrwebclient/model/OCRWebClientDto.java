833c762c41cb732eeb768431792c16b6706de260
boyunsim <tlaqhdbs12@gmail.com>
2025-02-21 13:04:32 +0900

    2025-02-21 ocr 이름으로 구분 하기 전까지 완료, 방문자수 집계 완료

---
+++ src/main/java/com/green/jobdone/ocrwebclient/model/OCRWebClientDto.java
@@ -0,0 +1,14 @@
+package com.green.jobdone.ocrwebclient.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+
+@Getter
+@Setter
+@Slf4j
+public class OCRWebClientDto {
+    private String businessName;
+    private String address;
+    private String busiCreatedAt;
+}
