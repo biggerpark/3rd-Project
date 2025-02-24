e5c56952429d5c9b8324bea9e36d632f5b1a1f57
yoonseoq <sysooo18@naver.com>
2025-02-24 18:17:05 +0900

    죽을 정도는 아니고 소정의 잘못을 했습니다

--- src/main/java/com/green/jobdone/business/model/BusinessDetailPutReq.java
+++ src/main/java/com/green/jobdone/business/model/BusinessDetailPutReq.java
@@ -21,12 +21,6 @@ public class BusinessDetailPutReq {
     private String closingTime;
 
 
-    @Schema(title = "업체 간단 설명", example = "벌레 잡아드립니다")
-    private String title;
-
-    @Schema(title = "업체 상세 설명", example = "겨울방학이라서 집벌레 많죠? 잡아드림.")
-    private String contents;
-
     @Schema(title = "전번" , example = "01055555555")
     private String tel;
 }
