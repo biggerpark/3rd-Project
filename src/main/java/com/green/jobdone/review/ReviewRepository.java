05ca6f9ef658809c2e8c5562545a4c897e3c34f2
boyunsim <tlaqhdbs12@gmail.com>
2025-02-26 19:18:53 +0900

    2025-02-26 JPA migration 완료

---
+++ src/main/java/com/green/jobdone/review/ReviewRepository.java
@@ -0,0 +1,52 @@
+package com.green.jobdone.review;
+
+import com.green.jobdone.entity.Review;
+import com.green.jobdone.review.comment.model.ReviewCommentGetRes;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+    @Query("SELECT d.user.userId FROM Review a JOIN a.service b JOIN b.product c JOIN c.business d WHERE a.reviewId = :reviewId")
+    Long findBusinessUserIdByReviewId(Long reviewId);
+
+    @Query("SELECT b.user.userId FROM Review a JOIN a.service b WHERE a.reviewId = :reviewId")
+    Long findUserIdByReviewId(Long reviewId);
+
+    @Query("SELECT a.user.userId FROM Service a WHERE a.serviceId = :serviceId")
+    Long findUserIdByServiceId(Long serviceId);
+
+    @Query("SELECT c.commentId, c.contents, c.createdAt, c.updatedAt, " +
+            "u.userId, u.name, b.logo, b.businessId " +
+            "FROM Comment c " +
+            "JOIN c.review r " +
+            "JOIN r.service s " +
+            "JOIN s.product p " +
+            "JOIN p.business b " +
+            "JOIN b.user u " +
+            "WHERE r.reviewId = :reviewId")
+    ReviewCommentGetRes selReviewCommentByReviewId(Long reviewId);
+
+/*
+        SELECT A.commentId
+        , A.`contents`
+        , A. createdAt
+        , A. updatedAt
+        , F.userId AS writerUserId
+        , F.name
+        , E.logo
+        , E.businessId
+        FROM comment A
+        JOIN review B
+        ON A.reviewId = B.reviewId
+        JOIN service C
+        ON C.serviceId = B.serviceId
+        JOIN product D
+        ON D.productId = C.productId
+        JOIN business E
+        ON E.businessId = D.businessId
+        JOIN user F
+        ON F.userId = E.userId
+        WHERE B.reviewId = #{reviewId}
+ */
+
+}
--- src/main/java/com/green/jobdone/review/comment/ReviewCommentService.java
+++ src/main/java/com/green/jobdone/review/comment/ReviewCommentService.java
@@ -3,6 +3,11 @@ package com.green.jobdone.review.comment;
 import com.green.jobdone.common.exception.CustomException;
 import com.green.jobdone.common.exception.ReviewErrorCode;
 import com.green.jobdone.config.security.AuthenticationFacade;
+import com.green.jobdone.entity.Comment;
+import com.green.jobdone.entity.Review;
+import com.green.jobdone.entity.User;
+import com.green.jobdone.review.CommentRepository;
+import com.green.jobdone.review.ReviewRepository;
 import com.green.jobdone.review.comment.model.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -14,31 +19,54 @@ import org.springframework.stereotype.Service;
 public class ReviewCommentService {
     private final ReviewCommentMapper reviewCommentMapper;
     private final AuthenticationFacade authenticationFacade;
+    private final CommentRepository commentRepository;
+    private final ReviewRepository reviewRepository;
 
     public long postReviewComment(ReviewCommentPostReq p) {
-        if(reviewCommentMapper.selUserIdByReviewId(p.getReviewId()) != authenticationFacade.getSignedUserId()) {
+        if(reviewRepository.findBusinessUserIdByReviewId(p.getReviewId()) != authenticationFacade.getSignedUserId()) {
             throw new CustomException(ReviewErrorCode.FAIL_TO_REG_COMMENT);
         }
-        ReviewCommentGetRes res = reviewCommentMapper.selReviewCommentByReviewId(p.getReviewId());
+        ReviewCommentGetRes res = reviewRepository.selReviewCommentByReviewId(p.getReviewId());
         if(res != null) {
             throw new CustomException(ReviewErrorCode.FAIL_TO_REG_COMMENT_EXIST);
         }
-        p.setUserId(authenticationFacade.getSignedUserId());
-        reviewCommentMapper.insReviewComment(p);
-        return p.getCommentId();
+//        p.setUserId(authenticationFacade.getSignedUserId());
+        Review review = new Review();
+        review.setReviewId(p.getReviewId());
+        User user = new User();
+        user.setUserId(authenticationFacade.getSignedUserId());
+        Comment comment = new Comment();
+        comment.setReview(review);
+        comment.setContents(p.getContents());
+        comment.setUser(user);
+        Comment savedComment = commentRepository.save(comment);
+//        reviewCommentMapper.insReviewComment(p);
+        return savedComment.getCommentId();
     }
 
     public ReviewCommentGetRes selReviewCommentByReviewId(ReviewCommentGetReq p) {
-        return reviewCommentMapper.selReviewCommentByReviewId(p.getReviewId());
+        return reviewRepository.selReviewCommentByReviewId(p.getReviewId());
     }
 
-    public int updReviewComment(ReviewCommentUpdReq p) {
+    public String updReviewComment(ReviewCommentUpdReq p) {
         p.setUserId(authenticationFacade.getSignedUserId());
-        return reviewCommentMapper.updReviewComment(p);
+        Long commentId = commentRepository.getCommentIdById(p.getUserId(), p.getReviewId());
+        Review review = new Review();
+        review.setReviewId(p.getReviewId());
+        User user = new User();
+        user.setUserId(p.getUserId());
+        Comment comment = commentRepository.findById(commentId).orElse(null);
+        comment.setContents(p.getContents());
+        comment.setReview(review);
+        comment.setUser(user);
+        Comment comment1 = commentRepository.save(comment);
+        return comment1.getContents();
     }
 
-    public int delReviewComment(ReviewCommentDelReq p) {
+    public void delReviewComment(ReviewCommentDelReq p) {
         p.setUserId(authenticationFacade.getSignedUserId());
-        return reviewCommentMapper.delReviewComment(p);
+        Long commentId = commentRepository.getCommentIdById(p.getUserId(), p.getReviewId());
+        Comment comment = commentRepository.findById(commentId).orElse(null);
+        commentRepository.delete(comment);
     }
 }
