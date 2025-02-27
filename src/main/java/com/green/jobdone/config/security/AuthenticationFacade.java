7cc43891dd3654417fe3dc0437182931cad3c26b
jin <hn04223@gmail.com>
2025-02-27 16:19:31 +0900

    2025 02 27 jwt 안쓰도록

--- src/main/java/com/green/jobdone/config/security/AuthenticationFacade.java
+++ src/main/java/com/green/jobdone/config/security/AuthenticationFacade.java
@@ -1,6 +1,7 @@
 package com.green.jobdone.config.security;
 
 import com.green.jobdone.config.jwt.JwtUser;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
@@ -15,5 +16,17 @@ public class AuthenticationFacade {
 
     public long getSignedUserId() {
         return getSignedUser().getSignedUserId();
+    }
+
+    public String getToken() {
+        // 현재 인증된 사용자의 Authentication 객체를 가져옵니다.
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        // Authentication 객체에서 JWT 토큰을 가져옵니다.
+        if (authentication != null && authentication.getCredentials() != null) {
+            return authentication.getCredentials().toString();  // 토큰을 반환
+        }
+
+        return null;  // 인증되지 않은 경우 null 반환
     }
 }
