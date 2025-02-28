01593245ed5f4326d33eddaeaad7f5270f0eaddd
jin <hn04223@gmail.com>
2025-02-28 16:52:51 +0900

    2025 02 28 서버시간 확인용

---
+++ src/main/java/com/green/jobdone/common/TestTimeZone.java
@@ -0,0 +1,7 @@
+package com.green.jobdone.common;
+
+public class TestTimeZone {
+    public static void main(String[] args) {
+        System.out.println("Current TimeZone: " + System.getProperty("user.timezone"));
+    }
+}
