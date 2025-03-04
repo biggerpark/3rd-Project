e9e62e2143d87ecda284a49b14d2d85507d62a56
jin <hn04223@gmail.com>
2025-03-04 15:54:51 +0900

    2025 03/04 message 못받아오던거 수정

--- src/main/java/com/green/jobdone/room/chat/model/ChatGetRes.java
+++ src/main/java/com/green/jobdone/room/chat/model/ChatGetRes.java
@@ -3,11 +3,13 @@ package com.green.jobdone.room.chat.model;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.util.List;
 
 @Getter
 @Setter
+@ToString
 public class ChatGetRes {
     private int flag;
     private long chatId;
