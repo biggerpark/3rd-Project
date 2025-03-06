968b0db21c314969b3834469360d7bfb61d2ac1b
ParkJunHyeong <parkhung007@gmail.com>
2025-03-06 17:07:58 +0900

    업체승인시 유저 state 변경 진행중

--- src/main/java/com/green/jobdone/admin/AdminService.java
+++ src/main/java/com/green/jobdone/admin/AdminService.java
@@ -5,8 +5,11 @@ import com.green.jobdone.business.BusinessRepository;
 import com.green.jobdone.category.CategoryRepository;
 import com.green.jobdone.category.DetailTypeRepository;
 import com.green.jobdone.common.PicUrlMaker;
+import com.green.jobdone.config.jwt.UserRole;
 import com.green.jobdone.entity.Business;
+import com.green.jobdone.entity.User;
 import com.green.jobdone.service.ServiceRepository;
+import com.green.jobdone.user.UserRepository;
 import com.green.jobdone.visitor.VisitorCountRepository;
 import com.green.jobdone.visitor.VisitorHistoryRepository;
 import jakarta.persistence.EntityNotFoundException;
@@ -14,6 +17,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import reactor.netty.udp.UdpServer;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
@@ -33,6 +37,7 @@ public class AdminService {
     private final VisitorCountRepository visitorCountRepository;
     private final CategoryRepository categoryRepository;
     private final DetailTypeRepository detailTypeRepository;
+    private final UserRepository userRepository;
 
 
     public List<BusinessApplicationGetRes> getBusinessApplication(int page) {
@@ -75,6 +80,11 @@ public class AdminService {
 
         LocalDate today = LocalDate.now();
 
+        Optional<User> user=userRepository.findById(business.getUser().getUserId());
+
+        User user1=user.get();
+        user1.setRole(UserRole.PRESIDENT);
+        userRepository.save(user1);
 
 
 
