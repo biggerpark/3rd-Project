078a7792d6c0184d4fc4835cd0bed8facdfa5229
boyunsim <tlaqhdbs12@gmail.com>
2025-03-17 17:27:27 +0900

    2025-03-17 메인에서 쓸 리뷰 리스트 추가3

--- src/main/java/com/green/jobdone/review/model/ReviewGetMainRes.java
+++ src/main/java/com/green/jobdone/review/model/ReviewGetMainRes.java
@@ -1,5 +1,6 @@
 package com.green.jobdone.review.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
@@ -12,13 +13,16 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @EqualsAndHashCode
 public class ReviewGetMainRes {
+    @JsonIgnore
+    private Long userId;
     private String name;
     private String pic;
     private Double score;
     private String contents;
     private LocalDateTime createdAt;
 
-    public ReviewGetMainRes(String name, String pic, Double score, String contents, LocalDateTime createdAt) {
+    public ReviewGetMainRes(Long userId, String name, String pic, Double score, String contents, LocalDateTime createdAt) {
+        this.userId = userId;
         this.name = name;
         this.pic = pic;
         this.score = score;
